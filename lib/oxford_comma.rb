13e8a42a3896527cbd210d96e449b31e96d6b110
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-09 18:48:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,26 @@
 def oxford_comma(array)
   
-  if array.size == 1
-    return array.first
-  elsif array.size == 2
-    return array.join(" and ")
-  elsif array.size == 3
+  # if array.size == 1
+  #   return array.first
+  # elsif array.size == 2
+  #   return array.join(" and ")
+  # elsif array.size == 3
     
-    array[-1] = "and #{array[-1]}"
+  #   array[-1] = "and #{array[-1]}"
     
-    array.join(", ") 
+  #   array.join(", ") 
     
-  else
-    array[-1] = "and #{array[-1]}"
-    array.join(", ")
-  end
+  # else
+  #   array[-1] = "and #{array[-1]}"
+  #   array.join(", ")
+  # end
+  
+  size = array.size
+  
+  case 
+
+  
+  
+  
   
 end
