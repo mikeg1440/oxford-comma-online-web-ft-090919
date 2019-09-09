379a373be17242aaf43f6b97f932153390ac0149
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-09 19:05:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,32 @@
 def oxford_comma(array)
-
+  
+  # if array.size == 1
+  #   return array.first
+  # elsif array.size == 2
+  #   return array.join(" and ")
+  # elsif array.size == 3
+    
+  #   array[-1] = "and #{array[-1]}"
+    
+  #   array.join(", ") 
+    
+  # else
+  #   array[-1] = "and #{array[-1]}"
+  #   array.join(", ")
+  # end
+  
+  size = array.size
+  
+  case size
+  when 1
+    array.first
+  
+  when 2
+    array.join(" and ")
+  
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
+  
 end
