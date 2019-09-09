2c1b0dbb98acfe7a50e1b5adf1e4db8a62167250
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-09 18:38:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,9 @@ def oxford_comma(array)
   if array.size == 2
     return array.join(" and ")
   elsif array.size == 3
-    str =  array.join(", ")  
+    str =  array.join(", ") 
+    
+    
 
   end
   
